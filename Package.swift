493e3966796f7403461509649c223efd45a439c9
MaxOl <some@mail.com>
2022-07-08 22:05:59 +0300

    init

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+import PackageDescription
+
+let package = Package(
+    name: "YandexMapsMobile",
+    platforms: [
+        .iOS(.v12),
+    ],
+    products: [
+        .library(
+            name: "YandexMapsMobile",
+            targets: ["YandexMapsMobile"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .binaryTarget(
+            name: "YandexMapsMobile",
+            url: "https://github.com/max270598/YandexMapsMobileZip/releases/download/2.0.0/YandexMapsMobile.xcframework.zip",
+            checksum: "2af1813d655ac552f84fb3e733cbae8aec8428db69081289cae357278641efc8"
+        ),
+    ]
+)
